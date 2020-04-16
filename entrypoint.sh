c8a9a6db302b8023ab73e51639162959b6f82511
André Roaldseth <andre.roaldseth@nav.no>
2020-04-16 09:47:37 +0200

    Make entrypoint verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+
+set -x
 
 cd "$GITHUB_WORKSPACE"
-
-export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 ktlint --reporter=checkstyle \
   | reviewdog -f=checkstyle -name="ktlint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
